efbff80080f2a4727d5d99d6036584220099ebc7
Mark Kim <viking617617@gmail.com>
2017-10-14 21:13:50 +0900

    added url, view TweetView(CreateView) to create a tweet

--- getstream/config/urls.py
+++ getstream/config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from stream_twitter import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^tweet/', login_required(views.TweetView.as_view()))
 ]
--- getstream/stream_twitter/views.py
+++ getstream/stream_twitter/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from stream_twitter.models import Tweet
+from stream_twitter.models import Follow
 
-# Create your views here.
+from django.views.generic.edit import CreateView
+
+class TweetView(CreateView):
+    model = Tweet
+    fields = ['text']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TweetView, self).form_valid(form)
+
+
+
+
+
